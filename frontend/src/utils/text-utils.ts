16880df95a85ca342fbca2820cdbc5616ee8b38f
Nicolas <nicolasbermudez666@gmail.com>
2024-09-18 21:35:39 -0300

    Remove code

--- frontend/src/utils/text-utils.ts
+++ frontend/src/utils/text-utils.ts
@@ -1,20 +1,5 @@
 import Phaser from 'phaser';
 
-/**
- * @typedef AnimateTextConfig
- * @type {object}
- * @property {() => void} [callback]
- * @property {number} [delay=25]
- */
-
-/**
- *
- * @param {Phaser.Scene} scene the Phaser 3 Scene the time event will be added to
- * @param {Phaser.GameObjects.Text} target the Phaser 3 Text Game Object that will be animated
- * @param {string} text the text to display on the target game object
- * @param {AnimateTextConfig} [config]
- * @returns {void}
- */
 export function animateText(scene : Phaser.Scene, target: Phaser.GameObjects.Text, text: string, config : any) {
   const length = text.length;
   let i = 0;
