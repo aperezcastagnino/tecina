d35c3ceab28bcea39a40c6052c06015d734fcb5e
paula <paulacianelli@hotmail.com>
2024-10-29 21:59:24 -0300

    fix

---
+++ frontend/src/main.ts
@@ -0,0 +1,22 @@
+import { Game, type Types } from "phaser";
+import { GAME_DIMENSIONS } from "./config/config";
+import { Boot } from "./scenes/boot";
+import { Level1 } from "./scenes/level1";
+import { MainMenu } from "./scenes/main-menu";
+import { Preloader } from "./scenes/preloader";
+
+const config: Types.Core.GameConfig = {
+  type: Phaser.CANVAS,
+  pixelArt: false, // averiguar
+  backgroundColor: "#000F00",
+  scale: {
+    parent: "game-container",
+    width: GAME_DIMENSIONS.WIDTH,
+    height: GAME_DIMENSIONS.HEIGHT,
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  scene: [Boot, Preloader, MainMenu, Level1],
+};
+
+export default new Game(config);
