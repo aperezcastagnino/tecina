0b5e2dc35e4667afd7511bd5f7fa7c3a77166c45
paula <paulacianelli@hotmail.com>
2024-11-03 11:09:29 -0300

    fix

--- frontend/src/scenes/map-level.ts
+++ frontend/src/scenes/map-level.ts
@@ -1,8 +1,10 @@
 import Phaser from "phaser";
 import { SceneKeys } from "./scene-keys";
-import { mapWidth, mapHeight, blockSize } from "../assets/constants";
+import { mapWidth, mapHeight } from "../assets/constants";
 import { mapColors } from "../assets/colors";
 import { MapLogicalGenerator } from "../map/map-logical-generation";
+import { TILE_SIZE } from "../config/config";
+
 
 const mapLogicalGenerator = new MapLogicalGenerator(mapWidth, mapHeight);
 const map = mapLogicalGenerator.run();
@@ -12,17 +14,13 @@ export class MapLevel extends Phaser.Scene {
     super(SceneKeys.MAP_LEVEL);
   }
 
-  preload() {
-    this.load.image("blue", "assets/tree.png"); // in progress
-  }
-
   create() {
     const rows = map.length;
     const columns = map[0]?.length || 0;
 
     // Calculates the start position of the board
-    const startX = (this.scale.width - columns * blockSize) / 2;
-    const startY = (this.scale.height - rows * blockSize) / 2;
+    const startX = (this.scale.width - columns * TILE_SIZE) / 2;
+    const startY = (this.scale.height - rows * TILE_SIZE) / 2;
 
     for (let n = 0; n < rows; n += 1) {
       for (let m = 0; m < columns; m += 1) {
@@ -30,15 +28,15 @@ export class MapLevel extends Phaser.Scene {
         const color = mapColors[hexa] || 0xffffff; // Get the color
 
         // Calculate the position of the cell
-        const x = startX + m * blockSize;
-        const y = startY + n * blockSize;
+        const x = startX + m * TILE_SIZE;
+        const y = startY + n * TILE_SIZE;
 
         // Add a rectangle with the specified color
         this.add.rectangle(
-          x + blockSize / 2, // where to start
-          y + blockSize / 2, // where to start
-          blockSize - 2, // rectangle width
-          blockSize - 2, // rectangle height
+          x + TILE_SIZE / 2, // where to start
+          y + TILE_SIZE / 2, // where to start
+          TILE_SIZE - 2, // rectangle width
+          TILE_SIZE - 2, // rectangle height
           color,
         );
       }
