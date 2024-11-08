db0495bf49a659ba4800d2b9217267ef5130a74a
paula <paulacianelli@hotmail.com>
2024-11-07 22:03:28 -0300

    Adding explanations

--- frontend/src/common/map/map-logical-generation.ts
+++ frontend/src/common/map/map-logical-generation.ts
@@ -63,6 +63,8 @@ export class MapLogicalGenerator {
     }
 
     // First border
+    // This sets the border of the map to 1
+
     for (let i = 0; i < this.#row; i += 1) {
       this.#map.mapTiles[i]![0] = 1;
       this.#map.mapTiles[i]![this.#column - 1] = 1;
@@ -73,7 +75,13 @@ export class MapLogicalGenerator {
       this.#map.mapTiles[this.#row - 1]![j] = 1;
     }
 
-    // Second border with random values
+    // Second border (inner border) with 1s randomly
+
+    // Here we use Math.floor(Math.random() * 2) to randomly set the inner border to 1
+    // Math.random() returns a random number between 0 and 1
+    // If we do Math.floor(Math.random()) returns always 0
+    // So we multiply by 2 to get a number between 0 and 2 and the floor returns 0 or 1
+
     for (let i = 1; i < this.#row - 1; i += 1) {
       this.#map.mapTiles[i]![1] = Math.floor(Math.random() * 2);
       this.#map.mapTiles[i]![this.#column - 2] = Math.floor(Math.random() * 2);
@@ -85,39 +93,35 @@ export class MapLogicalGenerator {
     }
   }
 
-  // eslint-disable-next-line class-methods-use-this
-  #calculateCumulativeFrequency(frequencies: number[]) {
+  fillMap() {
     const cumulativeFrequency = [];
     let total = 0;
 
-    frequencies.reduce((acc, weight) => {
-      // this is the same as the for loop
-      total = acc + weight; // total is the sum of all the weights
+    // Calculate the cumulative frequency
+    // if the frequency is [5, 10, 20] the cumulative frequency is [5, 15, 35]
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const weight of frequency) {
+      total += weight;
       cumulativeFrequency.push(total);
-      return total;
-    }, 0);
-    cumulativeFrequency.push(total); // cumulativeFrequency is the sum of all the weights
-    return cumulativeFrequency;
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  #getSelectedIndex(cumulativeFrequency: number[], randomValue: number) {
-    return cumulativeFrequency.findIndex((weight) => randomValue < weight);
-  }
-
-  fillMap() {
-    const cumulativeFrequency = this.#calculateCumulativeFrequency(frequency);
+    }
+    const maxFrequency = cumulativeFrequency[cumulativeFrequency.length - 1]!;
 
     for (let i = 0; i < this.#row; i += 1) {
       for (let j = 0; j < this.#column; j += 1) {
+        // If the column is empty, fill it with a random value
         if (this.#map.mapTiles[i]![j] === 0) {
-          const randomValue =
-            Math.random() *
-            cumulativeFrequency[cumulativeFrequency.length - 1]!;
-          const selectedIndex = this.#getSelectedIndex(
-            cumulativeFrequency,
-            randomValue,
+          // We pick the random value based on the cumulative frequency
+          // We multiply by the maxFrequency to get a value between 0 and the maxFrequency
+          const randomValue = Math.random() * maxFrequency;
+
+          // Once we have the random value we find the index of the first weight that is greater than the random value
+          // This is the weight that we will use to fill the map
+          const selectedIndex = cumulativeFrequency.findIndex(
+            (weight) => randomValue < weight,
           );
+
+          // We fill the map with the selected number
           this.#map.mapTiles[i]![j] = mapTiles[selectedIndex]!;
         }
       }
