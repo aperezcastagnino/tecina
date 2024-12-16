1cf155dd316f49a16832b7a23431eb3cb4eae8bd
Pei <perezcastagnino.andres@gmail.com>
2024-12-16 13:46:34 -0300

    save

--- frontend/src/scenes/level1.ts
+++ frontend/src/scenes/level1.ts
@@ -25,7 +25,7 @@ export class Level1 extends BaseScene {
     super.create();
 
     this._hideElements(
-      this._map.assetGroups.get(AssetKeys.ITEMS.FRUITS.ORANGE.NAME)!,
+      this._map.assetGroups.get(AssetKeys.ITEMS.FRUITS.ORANGE.NAME)!
     );
   }
 
@@ -35,7 +35,7 @@ export class Level1 extends BaseScene {
     this.anims.create({
       key: AnimationsKeys.ORANGE,
       frames: this.anims.generateFrameNumbers(
-        AssetKeys.ITEMS.FRUITS.ORANGE.NAME,
+        AssetKeys.ITEMS.FRUITS.ORANGE.NAME
       ),
       frameRate: 19,
       repeat: -1,
@@ -45,7 +45,7 @@ export class Level1 extends BaseScene {
   _defineBehaviors() {
     const treeGroup = this._map.assetGroups.get(AssetKeys.TILES.TREE)!;
     const orangeGroup = this._map.assetGroups.get(
-      AssetKeys.ITEMS.FRUITS.ORANGE.NAME,
+      AssetKeys.ITEMS.FRUITS.ORANGE.NAME
     )!;
     const npcGroup = this._map.assetGroups.get(AssetKeys.CHARACTERS.NPC)!;
 
@@ -55,7 +55,7 @@ export class Level1 extends BaseScene {
 
     this.physics.add.collider(this._player, npcGroup, (_player, npc) => {
       const npcSprite = npc as Phaser.GameObjects.Sprite;
-      this.#defineBehaviorForNPCs(npcSprite, orangeGroup);
+      this.#defineBehaviorForNPCs(npcSprite);
     });
 
     this.physics.add.collider(this._player, orangeGroup, (_player, item) => {
@@ -64,19 +64,19 @@ export class Level1 extends BaseScene {
     });
   }
 
-  #defineBehaviorForNPCs(
-    npc: Phaser.GameObjects.Sprite,
-    itemGroup: GameObjects.Group,
-  ) {
+  #defineBehaviorForNPCs(npc: Phaser.GameObjects.Sprite) {
     this.#total_oranges = this._map.assetGroups
-      .get("ORANGE")!
-      .getChildren().length;
+      .get(AssetKeys.ITEMS.FRUITS.ORANGE.NAME)!.getLength();
+
     if (this._controls.wasSpaceKeyPressed()) {
       if (npc.name === "npc-1") {
+        const orangeGroup = this._map.assetGroups.get(
+          AssetKeys.ITEMS.FRUITS.ORANGE.NAME)!;
+
         if (this.#npc_1_show_first_message) {
-          this._dialog?.show("npc-1");
-          this._showElements(itemGroup!);
-          this._dialog?.setMessageComplete("npc-1");
+          this._dialog?.show(npc.name);
+          this._showElements(orangeGroup!);
+          this._dialog?.setMessageComplete(npc.name);
           this.#npc_1_show_first_message = false;
         } else if (this.#total_oranges > 0) {
           this._dialog?.show("npc-1");
@@ -84,18 +84,19 @@ export class Level1 extends BaseScene {
         } else {
           if (this.#npc_1_show_first_complete_collect_objects) {
             if (!this.#npc_1_show_intermediate_message) {
-              this._dialog?.setMessageComplete("npc-1");
+              this._dialog?.setMessageComplete(npc.name);
             }
             this.#npc_1_show_first_complete_collect_objects = false;
           }
-          this._dialog?.show("npc-1");
+          this._dialog?.show(npc.name);
         }
       }
+
       if (npc.name === "npc-2") {
         if (this.#total_oranges === 0) {
-          this._dialog?.setMessageComplete("npc-2");
+          this._dialog?.setMessageComplete(npc.name);
         }
-        this._dialog?.show("npc-2");
+        this._dialog?.show(npc.name);
       }
     }
   }
