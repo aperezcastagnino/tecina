0d12e4ce3c05462f8db97bc42c82c4c0e3236eb8
paula <paulacianelli@hotmail.com>
2024-12-26 16:33:04 -0300

    tranlsate comments

--- frontend/src/common/map/map-generator.ts
+++ frontend/src/common/map/map-generator.ts
@@ -176,7 +176,7 @@ export class MapGenerator {
     };
 
     console.log("Room creado!!");
-    console.log(newPartition.room); // Verifica room en el nuevo objeto
+    console.log(newPartition.room); // Check room in the new object
     return newPartition;
   }
 
@@ -185,7 +185,7 @@ export class MapGenerator {
 
     if (!partition.left && !partition.right) {
       const updatedPartition = this.createRoom(partition);
-      newPartition.room = updatedPartition.room; // Asignar el room al objeto original
+      newPartition.room = updatedPartition.room; // Assign the room to the original object
     } else {
       if (partition.left) {
         newPartition.left = this.assignRooms(partition.left);
@@ -233,10 +233,10 @@ export class MapGenerator {
             this.#map.startPosition.x = x;
             this.#map.startPosition.y = y;
           }
-          this.#map.tiles[y]![x] = this.getRandomBasedOnFrequency(
+            this.#map.tiles[y]![x] = this.getRandomBasedOnFrequency(
             frecuency,
             assets,
-          ); // Reemplaza solo celdas vacías
+            ); // Replace only empty cells
         }
       }
     }
