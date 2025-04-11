943decf98bd47fe2d07be6cf651e880d03878950
Pei <perezcastagnino.andres@gmail.com>
2025-04-11 15:17:34 -0300

    chore: deploy configuration done

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,9 +7,6 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/tecina/",
-  build: {
-    outDir: "dist",
-  },
   resolve: {
     alias: {
       $assets: resolve("./src/assets"),
