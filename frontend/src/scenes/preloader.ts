6192d54f47794f1d6d4cd006785406ca055107de
Nicolas <nicolasbermudez666@gmail.com>
2025-05-26 07:23:20 -0300

    resolve conflicts

--- frontend/src/scenes/preloader.ts
+++ frontend/src/scenes/preloader.ts
@@ -136,7 +136,6 @@ export default class Preloader extends Scene {
       "/ui-components/shadow-button.png",
     );
     this.load.image(UIComponentKeys.CROSS, "/ui-components/cross.png");
-
     this.load.image(
       UIComponentKeys.YES_BUTTON,
       "/ui-components/yes-button.png",
