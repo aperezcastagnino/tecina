6bd1ede2d9d78a43ee53855b62dc78479d0a041c
Paula <65620266+paulacianelli@users.noreply.github.com>
2025-06-14 14:48:32 -0300

    feat(assets): add new assets for final levels and level indicators (#224)
    * Adding levels and assets
    
    * folder
    
    * Fix
    
    * Remove level 9
    
    * fix

--- frontend/src/scenes/levels/levels-config.ts
+++ frontend/src/scenes/levels/levels-config.ts
@@ -12,38 +12,49 @@ export const levelsConfig: LevelMetadata[] = [
   {
     key: SceneKeys.LEVEL_2,
     position: { x: 450, y: 620 },
-    nextLevel: [SceneKeys.LEVEL_3],
+    nextLevel: [SceneKeys.LEVEL_3, SceneKeys.LEVEL_4],
     previousLevel: [SceneKeys.LEVEL_1],
   },
   {
     key: SceneKeys.LEVEL_3,
     position: { x: 820, y: 500 },
-    nextLevel: [SceneKeys.LEVEL_4, SceneKeys.LEVEL_5],
+    nextLevel: [SceneKeys.LEVEL_5, SceneKeys.LEVEL_6, SceneKeys.LEVEL_7],
     previousLevel: [SceneKeys.LEVEL_2],
   },
   {
     key: SceneKeys.LEVEL_4,
-    position: { x: 690, y: 260 },
-    nextLevel: [SceneKeys.LEVEL_5],
-    previousLevel: [SceneKeys.LEVEL_3],
+    position: { x: 370, y: 880 },
+    nextLevel: [],
+    previousLevel: [SceneKeys.LEVEL_2],
   },
   {
     key: SceneKeys.LEVEL_5,
-    position: { x: 1370, y: 500 },
-    nextLevel: [SceneKeys.LEVEL_6],
+    position: { x: 690, y: 260 },
+    nextLevel: [],
     previousLevel: [SceneKeys.LEVEL_3],
   },
   {
     key: SceneKeys.LEVEL_6,
-    position: { x: 1510, y: 210 },
-    nextLevel: [SceneKeys.WIN_SCENE],
-    previousLevel: [SceneKeys.LEVEL_5],
+    position: { x: 1370, y: 500 },
+    nextLevel: [SceneKeys.LEVEL_8, SceneKeys.LEVEL_9],
+    previousLevel: [SceneKeys.LEVEL_3],
   },
-  /*
   {
     key: SceneKeys.LEVEL_7,
     position: { x: 940, y: 860 },
     nextLevel: [],
+    previousLevel: [SceneKeys.LEVEL_3],
   },
-  */
+  {
+    key: SceneKeys.LEVEL_8,
+    position: { x: 1510, y: 210 },
+    nextLevel: [],
+    previousLevel: [SceneKeys.LEVEL_6],
+  },
+  {
+    key: SceneKeys.LEVEL_9,
+    position: { x: 1600, y: 860 },
+    nextLevel: [SceneKeys.WIN_SCENE],
+    previousLevel: [SceneKeys.LEVEL_6],
+  },
 ];
